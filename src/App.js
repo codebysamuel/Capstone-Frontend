3824cb57fad676a389e4cc1d55a5b520aa247500
gouribhise <gouri.a.bhise@gmail.com>
2024-08-01 10:27:27 +0530

    installed bootstrap

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import './App.css';
-
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min";
 function App() {
   return (
-    <>
+    <div className='container'>
+      <div className='row'>
+      <div className='col-md-3'>
       <h1>Little lemon</h1>
-    </>
+
+      </div>
+      <div className='col-md-3'>
+      <h1>Little lemon</h1>
+
+      </div>
+      </div>
+     
+    </div>
   );
 }
 

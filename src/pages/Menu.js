3b414b9f2bd896c76e53c865e40a7b302050170d
gouribhise <gouri.a.bhise@gmail.com>
2024-08-07 21:41:54 +0530

    skeleton page for menu

--- src/pages/Menu.js
+++ src/pages/Menu.js
@@ -1,8 +1,54 @@
 function Menu(){
     return (
-        <>
-        <h1>Menu</h1>
-        </>
+        <div className="container">
+      <h1>Our Menu</h1>
+      <hr/> 
+      <div className="row">
+        <h3>Appetizers</h3>
+        <div className="col">
+            one
+        </div>
+        <div className="col">
+            two
+        </div>
+        <div className="col">
+            three
+        </div>
+        <div className="col">
+            More...
+        </div>
+      </div>
+      <div className="row">
+        <h3>Main Dishes</h3>
+        <div className="col">
+            one
+        </div>
+        <div className="col">
+            two
+        </div>
+        <div className="col">
+            three
+        </div>
+        <div className="col">
+            More...
+        </div>
+      </div>
+      <div className="row">
+        <h3>Desserts</h3>
+        <div className="col">
+            one
+        </div>
+        <div className="col">
+            two
+        </div>
+        <div className="col">
+            three
+        </div>
+        <div className="col">
+            More...
+        </div>
+      </div>
+        </div>
     )
 }
 

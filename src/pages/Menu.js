df6cf2306e402e278b4bdd9edd48dd3c00fad2f6
gouribhise <gouri.a.bhise@gmail.com>
2024-08-13 18:24:57 +0530

    fixed menu page

--- src/pages/Menu.js
+++ src/pages/Menu.js
@@ -8,53 +8,53 @@ function Menu(){
       <hr/> 
       <div className="row">
         <h3>Appetizers</h3>
-        <div className="col">
+        <div className="col-lg-3 col-md-6  col-sm-12 img-holder">
         <img src={ImageData[0]} width="100%" className="menu"/>
 
         </div>
-        <div className="col">
+        <div className="col-lg-3 col-md-6 col-sm-12 img-holder">
         <img src={ImageData[1]} width="100%" className="menu"/>
 
         </div>
-        <div className="col">
+        <div className="col-lg-3 col-md-6 col-sm-12 img-holder">
         <img src={ImageData[2]} width="100%" className="menu"/>
         </div>
-        <div className="col">
+        <div className="col-lg-3 col-md-6 col-sm-12 img-holder">
        <Link className="px-5"> <h3>More...</h3></Link>
         </div>
       </div>
       <div className="row">
         <h3>Main Dishes</h3>
-        <div className="col">
+        <div className="col-lg-3 col-md-6  col-sm-12 img-holder">
         <img src={ImageData[3]} width="100%" className="menu"/>
 
         </div>
-        <div className="col">
+        <div className="col-lg-3 col-md-6  col-sm-12 img-holder">
         <img src={ImageData[4]} width="100%" className="menu"/>
 
         </div>
-        <div className="col">
+        <div className="col-lg-3 col-md-6  col-sm-12 img-holder">
         <img src={ImageData[5]} width="100%" className="menu"/>
         </div>
-        <div className="col">
+        <div className="col-lg-3 col-md-6  col-sm-12 img-holder">
         <Link className="px-5"> <h3>More...</h3></Link>
         </div>
       </div>
       <div className="row">
         <h3>Desserts</h3>
-        <div className="col">
+        <div className="col-lg-3 col-md-6  col-sm-12 img-holder">
         <img src={ImageData[6]} width="100%" className="menu"/>
 
         </div>
-        <div className="col">
+        <div className="col-lg-3 col-md-6  col-sm-12 img-holder">
         <img src={ImageData[7]} width="100%" className="menu"/>
 
         </div>
-        <div className="col">
+        <div className="col-lg-3 col-md-6  col-sm-12 img-holder">
         <img src={ImageData[8]} width="100%" className="menu"/>
 
         </div>
-        <div className="col">
+        <div className="col-lg-3 col-md-6  col-sm-12 img-holder">
         <Link className="px-5"> <h3>More...</h3></Link>
         </div>
       </div>
